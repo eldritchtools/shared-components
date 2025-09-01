3a61cf3bd74e4fc9838970c119a5cee60a8bec83
eldritchtools <eldritch.tools@gmail.com>
2025-09-01 21:22:03 +0800

    Update to fix react not available

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,1 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-Object.defineProperty(exports, "Footer", {
-  enumerable: true,
-  get: function get() {
-    return _Footer["default"];
-  }
-});
-var _Footer = _interopRequireDefault(require("./Footer"));
-function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+export { default as Footer } from "./Footer";
